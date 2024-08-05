cefa69509e705fab5634c5c201792d863f4df0de
Luke Maurits <luke@maurits.id.au>
2024-08-05 15:50:35 +0200

    Tidy up data processing script a little bit, adding the participant demographics sanity check.

--- process_data.R
+++ process_data.R
@@ -1,7 +1,29 @@
 library(tidyverse)
 
+# Read separate data files for participant demographics and responses
 my_demogs <- read_csv("demogs.csv")
 my_responses <- read_csv("responses.csv")
+
+# Join the two tibbles into one, using the shared ID column as a key to link
+# rows
 my_data <- left_join(my_responses, my_demogs, by="ID")
 
+# Quick sanity check - is the number of unique participant IDs equal to the 
+# number of unique combinations of (ID, sex, age)?  Stop if not, to catch
+# manual data formatting bugs
+N_participants <- my_data %>%
+  pull(ID) %>%
+  unique() %>%
+  length()
+
+N_unique_demogs <- my_data %>%
+  select(ID, age, gender) %>%
+  unique() %>%
+  nrow()
+
+stopifnot(N_participants == N_unique_demogs)
+
+# Here we might do other data preparation tasks...
+
+# Save our results to a .csv file which will be read by our analysis script
 write_csv(my_data, "processed_data.csv")
