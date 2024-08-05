8b59a2c5cae5847eac13957df9c91097ebd45d4e
Luke Maurits <luke@maurits.id.au>
2024-08-05 14:37:34 +0200

    Join two Excel sheets, fit a model , make a plot.

--- my_script.R
+++ my_script.R
@@ -9,6 +9,14 @@ demographics <- my_data %>%
   filter(age > 10) %>%
   unique()
 
+
+
+
+
+my_demogs <- read_xlsx("data.xlsx", sheet="demogs")
+my_responses <- read_xlsx("data.xlsx", sheet="responses")
+my_data <- left_join(my_responses, my_demogs, by="ID")
+
 N_participants <- my_data %>%
   pull(ID) %>%
   unique() %>%
@@ -19,4 +27,13 @@ N_unique_demogs <- my_data %>%
   unique() %>%
   nrow()
 
-stopifnot(N_participants != N_unique_demogs)
+stopifnot(N_participants == N_unique_demogs)
+
+m <- glm(response ~ condition + gender + age, data=my_data,
+    family="binomial")
+
+my_data <- my_data %>%
+  mutate(predictions = predict(m, type="response"))
+
+ggplot(my_data) +
+  geom_point(aes(x=age, y=predictions, colour=condition))
